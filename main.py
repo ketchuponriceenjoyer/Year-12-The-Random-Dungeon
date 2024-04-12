8d32be11a80a8f6e4be6141a457d2dc7ba750197
ketchuponriceenjoyer <164109224+ketchuponriceenjoyer@users.noreply.github.com>
2024-04-12 09:33:34 +0000

    april 12
    error fixes on the restart function accidentally saves progress load and just makes it look like the player just got revived than starting over

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-import time
+#import random will the be used very often in this code
 import random
 
+#this is a lists of weapons that uses hashes to distrubte the weapon stats
 weapon = [
     {
         "name": "sword",
         "attack": {
-            "first_name": "light_attack",
-            "first_skill": random.randint(5, 10),
+            "first_name": "light_attack",#to display name
+            "first_skill": random.randint(8, 15),
             "second_name": "heavy_attack",
-            "second_skill": random.randint(10, 15)
+            "second_skill": random.randint(15, 17)#the attack hashes are a library of skills that the attack has that display their name and randomise what damage they can do
         }
     },  # 0
     {
         "name": "whip",
         "attack": {
             "first_name": "strike",
-            "first_skill": random.randint(3, 4),
+            "first_skill": random.randint(5, 8),
             "second_name": "rip",
-            "second_skill": random.randint(4, 12)
+            "second_skill": random.randint(20, 27)
         }
     },  # 1
     {
         "name": "staff",
         "attack": {
             "first_name": "fireball",
-            "first_skill": random.randint(5, 17),
+            "first_skill": random.randint(1, 7),
             "second_name": "lightning Strike",
-            "second_skill": random.randint(5, 20)
+            "second_skill": random.randint(20, 30)
         }
     },  # 2
     {
         "name": "bow",
         "attack": {
             "first_name": "light arrow",
-            "first_skill": random.randint(5, 10),
+            "first_skill": random.randint(1, 17),
             "second_name": "piercing arrow",
-            "second_skill": random.randint(1, 4)
+            "second_skill": random.randint(5, 25)
         }
     }  # 2
 ]
@@ -49,7 +50,7 @@ enemy_mob = [
           "mob_first_skill": random.randint(5, 10),
           "mob_second_name": "roundhouse",
           "mob_second_skill": random.randint(10, 20)
-      }
+      }#this hash index is used inside this list just in case if any error is occured
     },
     {
         "name": "goblin",
@@ -82,11 +83,13 @@ enemy_mob = [
         }
     },
 ]
-battles_won = 0
+battles_won = 0#this is a variable that is used for the game progression
+
 gem_shards = [
-  {"diamond_shard": False,"name": "diamond_shard"},
-  {"gold_shard": False,"name": "gold_shard"},
-  {"emerald_shard": False,"name": "emerald_shard"}
+  {"please":"work"},
+  {"diamond_shard": None,"gem_name": "diamond_shard"},
+  {"gold_shard": None,"gem_name": "gold_shard"},
+  {"emerald_shard": None,"gem_name": "emerald_shard"}
 ]
 
 player_status_inventory = [
@@ -94,6 +97,8 @@ player_status_inventory = [
     {"coins": 0},  #1
     {"weapon_1": None,"weapon_2": None},  #2
 ]
+
+
 def start_game():
   print("Welcome to the The Random Dungeon Game")
   #time.sleep(3.5)
@@ -112,20 +117,21 @@ def start_game():
   print(weapon[2]["name"])
   print("you can only pick 2 weapons")
   #time.sleep(3.5)
-  print("weapon choices", " 1 for sword", " 2 for Whimp", " 3 for staff")
+  print("weapon choices", " 1 for sword", " 2 for Whip", " 3 for staff")
   #time.sleep(2.5)
-  
+
   while True:
     try:
       Chosen_weapon_1 = int(input("Choose your first weapon -- "))
       #chosen sword for weapon1
       if Chosen_weapon_1 == 1:
+        
         player_status_inventory[1][
             "coins"] = player_status_inventory[1]["coins"] + random.randint(
                 5, 10)
         player_status_inventory[2]["weapon_1"] = weapon[0]
         break
-  
+
       elif Chosen_weapon_1 == 2:
         player_status_inventory[1][
             "coins"] = player_status_inventory[1]["coins"] + random.randint(
@@ -133,20 +139,20 @@ def start_game():
         player_status_inventory[2]["weapon_1"] = weapon[1]
         break
         #chosen whip for weapon1
-  
+
       elif Chosen_weapon_1 == 3:
         player_status_inventory[1][
             "coins"] = player_status_inventory[1]["coins"] + random.randint(
                 5, 10)
         player_status_inventory[2]["weapon_1"] = weapon[2]
         break
-  
+
         #chosen staff for weapon1
-  
+
     except ValueError:
       print("not in choices")
       continue
-  
+
   while True:
     try:
       Chosen_weapon_2 = int(input("Choose your second weapon -- "))
@@ -161,7 +167,7 @@ def start_game():
         player_status_inventory[2]["weapon_2"] = weapon[0]
         
         break
-  
+
       elif Chosen_weapon_2 == 2:
         player_status_inventory[1][
             "coins"] = player_status_inventory[1]["coins"] + random.randint(
@@ -170,7 +176,7 @@ def start_game():
         
         break
         #chosen whip for weapon1
-  
+
       elif Chosen_weapon_2 == 3:
         player_status_inventory[1][
             "coins"] = player_status_inventory[1]["coins"] + random.randint(
@@ -178,12 +184,13 @@ def start_game():
         player_status_inventory[2]["weapon_2"] = weapon[2]
         
         
-        
+
         break
-  
+
     except ValueError:
       print("not in choices")
       continue
+  
 start_game()
 
 def Player_status():
@@ -216,17 +223,16 @@ def Player_status():
         player_status_inventory[2]["weapon_2"]["attack"]["second_skill"], "]")
   print("-----------------------------------------------------------------------------------")
   print("Gem shards - ")
-  if gem_shards[0]["diamond_shard"] == True:
-    print("Diamond Shard")
-  if gem_shards[1]["gold_shard"] == True:
-    print("Gold Shard")
-  if gem_shards[2]["emerald_shard"] == True:
-    print("Emerald Shard")
-  
+  print("Shard 1:", gem_shards[1]["diamond_shard"])
+  print("Shard 2:", gem_shards[2]["gold_shard"])
+  print("Shard 3:", gem_shards[3]["emerald_shard"])
+  print("-----------------------------------------------------------------------------------")
+
 
 Player_status()
 
 def on_game_check_status():
+  
   while True:
     try:
       print("before you enter the next event, would you like to check your status? A for yes B for no or C for reset game")
@@ -238,36 +244,43 @@ def on_game_check_status():
       elif player_options == "B":
         situation_randomiser()
       elif player_options == "C":
+        player_status_inventory[0]["health"] = 100
+        player_status_inventory[1]["coins"] = 0
+        player_status_inventory[2]["weapon_1"] = None
+        player_status_inventory[2]["weapon_2"] = None
+        gem_shards[1]["diamond_shard"] = None
+        gem_shards[2]["gold_shard"] = None
+        gem_shards[3]["emerald_shard"] = None
         start_game()
     except ValueError:
       print("not in choices")
+  
 
 def battle():
   global enemy_mob 
   global weapon
-  enemy_selected = enemy_mob[random.randint(1, 3)]
-  enemy_use_weapon_or_not = random.randint(1,2)
-  enemy_weapon_selection = random.randint(0, 3)
+  global battles_won
+
+
   #Sometimes the enemy will have a health lower than zero for some reason so I added this for a backup just in case the enemy_mob mob fails to give it a good emount of health, this function will decide its health
-  if enemy_selected["health"] <= 0:
-    enemy_selected["health"] = random.randint(20, 35)
+
   print("-----------------------------------------------------------------------------------")
   print("                                -BATTLE ENCOUNTER-                                    ")
   print("-----------------------------------------------------------------------------------")
+ 
+         
+          
+  enemy_selected = enemy_mob[random.randint(1, 3)]
+  print(enemy_selected["attack"]["mob_first_name"], " : deals", enemy_selected["attack"]["mob_first_skill"],"damage")
+  print(enemy_selected["attack"]["mob_second_name"], " : deals", enemy_selected["attack"]["mob_second_skill"],"damage")
   print("you have encountered a", enemy_selected["name"])
+              
+  if enemy_selected["health"] <= 0:
+    enemy_selected["health"] = random.randint(20, 35)
   print(enemy_selected["health"], " : Health")
+
   
-  if enemy_use_weapon_or_not == 1:
-    enemy_selected["attack"] = weapon[enemy_weapon_selection]
-    print("It is using :", enemy_selected["attack"]["name"])
-    print(enemy_selected["attack"]["attack"]["first_name"], " : deals", enemy_selected["attack"]["attack"]["first_skill"],"damage")
-    print(enemy_selected["attack"]["attack"]["second_name"], " : deals", enemy_selected["attack"]["attack"]["second_skill"],"damage")
-  elif enemy_use_weapon_or_not == 2:
-    print(enemy_selected["attack"]["mob_first_name"], " : deals", enemy_selected["attack"]["mob_first_skill"],"damage")
-    print(enemy_selected["attack"]["mob_second_name"], " : deals", enemy_selected["attack"]["mob_second_skill"],"damage")
-  else:
-    print("Theres your problem")
-  
+
   print("-----------------------------------------------------------------------------------")
   while True:
     try:
@@ -280,14 +293,14 @@ def battle():
         on_game_check_status()
     except ValueError:
       print("not in choices")
-    
+
 
   while True:
     print("Players turn")
     print("-----------------------------------------------------------------------------------")
     player_damage_dealt = 0
     print("Choose which weapon you would like to use ot you use an")
-    print("(A)", player_status_inventory[2]["weapon_1"]["name"], "(B)")
+    print("(A)", player_status_inventory[2]["weapon_1"]["name"], "(B)", player_status_inventory[2]["weapon_2"]["name"])
     player_weapon_selection_choice = input(":  ").upper()
     if player_weapon_selection_choice == "A":
       weapon_skill_selection = random.randint(1,2)
@@ -300,7 +313,7 @@ def battle():
       if weapon_skill_selection == 2:
         player_damage_dealt = player_status_inventory[2]["weapon_1"]["attack"]["second_skill"]
         print("You use", player_status_inventory[2]["weapon_1"]["name"], "skill(",player_status_inventory[2]["weapon_1"]["attack"]["second_skill"],")", player_status_inventory[2]["weapon_1"]["attack"]["second_name"])
-        
+
     elif player_weapon_selection_choice == "B":
       print("-----------------------------------------------------------------------------------")
       print("Result")
@@ -315,17 +328,24 @@ def battle():
     else:
       print("not in choices")
       continue
-      
-    
+
+
     enemy_selected["health"] -= player_damage_dealt
     print("you dealt", player_damage_dealt, "Damage")
     if enemy_selected["health"] <= 0:
       print("0 : Health")
-      
+
     else:
       print(enemy_selected["health"], " : Enemy Health")
-
+    print("-----------------------------------------------------------------------------------")
     if enemy_selected["health"] <= 0:
+      battles_won = battles_won + 1
+      if battles_won == 3:
+        print("you have won battles")
+        battles_won = 0
+        crystal_end()
+        break
+        
       enemy_use_weapon_or_not = 0
       enemy_weapon_selection = 0
       reward = random.randint(5, 25)
@@ -333,26 +353,17 @@ def battle():
       print("you have defeated the", enemy_selected["name"])
       print("you have been awarded, ", player_status_inventory[1]["coins"])
       on_game_check_status()
-      break
+    
 
 
     enemy_damage_dealt = 0
-    if enemy_use_weapon_or_not == 1:
-      enemy_weapon_skill_selection = random.randint(1,2)
-      if enemy_weapon_skill_selection == 1:
-        enemy_damage_dealt = enemy_selected["attack"]["attack"]["first_skill"]
-        print("The enemy uses", enemy_selected["attack"]["attack"]["first_name"], "skill(",enemy_selected["attack"]["attack"]["first_skill"],")")
-      elif enemy_weapon_skill_selection == 2:
-        enemy_damage_dealt = enemy_selected["attack"]["attack"]["second_skill"]
-        print("The enemy uses", enemy_selected["attack"]["attack"]["second_name"], "skill(",enemy_selected["attack"]["attack"]["second_skill"],")")
-    if enemy_use_weapon_or_not == 2:
-      enemy_weapon_skill_selection = random.randint(1,2)
-      if enemy_weapon_skill_selection == 1:
-        enemy_damage_dealt = enemy_selected["attack"]["mob_first_skill"]
-        print("The enemy uses", enemy_selected["attack"]["mob_first_name"], "skill(",enemy_selected["attack"]["mob_first_skill"],")")
-      elif enemy_weapon_skill_selection == 2:
-        enemy_damage_dealt = enemy_selected["attack"]["mob_second_skill"]
-        print("The enemy uses", enemy_selected["attack"]["mob_second_name"], "skill(",enemy_selected["attack"]["mob_second_skill"],")")
+    enemy_weapon_skill_selection = random.randint(1,2)
+    if enemy_weapon_skill_selection == 1:
+      enemy_damage_dealt = enemy_selected["attack"]["mob_first_skill"]
+      print("The enemy uses", enemy_selected["attack"]["mob_first_name"], "skill(",enemy_selected["attack"]["mob_first_skill"],")")
+    elif enemy_weapon_skill_selection == 2:
+      enemy_damage_dealt = enemy_selected["attack"]["mob_second_skill"]
+      print("The enemy uses", enemy_selected["attack"]["mob_second_name"], "skill(",enemy_selected["attack"]["mob_second_skill"],")")
 
     player_status_inventory[0]["health"] -= enemy_damage_dealt
     print("The enemy dealt", enemy_damage_dealt, "Damage")
@@ -368,10 +379,17 @@ def battle():
             player_status_inventory[1]["coins"] = 0
             player_status_inventory[2]["weapon_1"] = None
             player_status_inventory[2]["weapon_2"] = None
+            gem_shards[1]["diamond_shard"] = None
+            gem_shards[2]["gold_shard"] = None
+            gem_shards[3]["emerald_shard"] = None
+        
+            
+
             start_game()
+            situation_randomiser()
             break
-      
-          
+
+
           elif reset_game_choice == "b":
             print("Thanks for playing")
             break  
@@ -384,8 +402,8 @@ def battle():
 
 
 def doctor():
-  medical_cost = random.randint(35,100)
-  medical_treatment = random.randint(5,35)
+  medical_cost = random.randint(10,100)
+  medical_treatment = random.randint(5,80)
   print("-----------------------------------------------------------------------------------")
   print("You find a small cavern")
   print("you enter the cavern and notice its a man")
@@ -416,7 +434,7 @@ def doctor():
           else:
             print("you do not have enough coins")
             continue
-            
+
       elif doctor_treatment_choice == "B":
         print("Get the hell out of my cavern!")
         on_game_check_status()
@@ -424,27 +442,38 @@ def doctor():
         print("three gems shards are scattered around the forest and legends say that they are the only way to escape the forest")
         print("If you get all three, an escape to the outside plains will appear")
         print("But you will need run quick though because these creatures are attracted to those shards")
+        print("in order to get them I heard you have to take down three creatures of any type to get one of the shards" )
     except ValueError:
       print("not in choices")
-      
+
 
 def crystal_end():
-  gem_selection = random.randint(0, 2)
-  
-  print("-----------------------------------------------------------------------------------")
-  if gem_selection == 0:
-    gem_shards[gem_selection]["diamond_shard"] = True
-  elif gem_selection == 1:
-    gem_shards[gem_selection]["gold_shard"] = True
-  elif gem_selection == 2:
-    gem_shards[gem_selection]["emerald_shard"] = True
-    
-  if gem_shards[2]["emerald_shard"] or gem_shards[1]["gold_shard"] or gem_shards[0]["diamond_shard"] == True:
-    gem_selection = random.randint(0, 3)
+  global gem_shards
+  gem_selection = random.randint(1, 3)
 
-  print("you found a ", gem_shards[gem_selection]["name"])
   print("-----------------------------------------------------------------------------------")
-  if gem_shards[2]["emerald_shard"] and gem_shards[1]["gold_shard"] and gem_shards[0]["diamond_shard"] == True:
+  if gem_selection == 1 and gem_shards[1]["diamond_shard"] == None:
+    
+    gem_shards[1]["diamond_shard"] = "Diamond_Shard"
+    
+  else:
+    gem_selection = random.randint(1, 3)
+  if gem_selection == 2 and gem_shards[2]["gold_shard"] == None:
+    
+    gem_shards[2]["gold_shard"] = "Gold_Shard"
+    
+  else:
+    gem_selection = random.randint(1, 3)
+  if gem_selection == 3 and gem_shards[3]["emerald_shard"] == None:
+    
+    gem_shards[3]["emerald_shard"] = "Emerald_Shard"
+    
+  else:
+    gem_selection = random.randint(1, 3)
+                                   
+  print("you found a ", gem_shards[gem_selection]["gem_name"])
+  print("-----------------------------------------------------------------------------------")
+  if gem_shards[3]["emerald_shard"] == "Emerald_Shard" and gem_shards[2]["gold_shard"] == "Gold_Shard" and gem_shards[1]["diamond_shard"] == "Diamond_Shard":
     print("-----------------------------------------------------------------------------------")
     print("you have found all the gem shards")
     print("you run as fast as you can, as you see mobs jump out of any direction, trying to get you")
@@ -463,23 +492,27 @@ def crystal_end():
         player_end_choice = input("Would you like to play again? A for yes B for no").upper()
         if player_end_choice == "A":
           start_game()
+          gem_shards[1]["diamond_shard"] = None
+          gem_shards[2]["gold_shard"] = None
+          gem_shards[3]["emerald_shard"] = None
+          player_status_inventory[0]["health"] = 100
+          player_status_inventory[1]["coins"] = 0
+          player_status_inventory[2]["weapon_1"] = None
+          player_status_inventory[2]["weapon_2"] = None
           break
         else:
           print("Thanks for playing")
           break
       except ValueError:
         print("not in choices")
-    
+
   else:
     on_game_check_status()
-    
-    
-  
 
 def situation_randomiser():
-  
-  Situation_randomiser = random.randint(0, 45)
 
+  Situation_randomiser = random.randint(0, 45)
+  
   
   if Situation_randomiser >= 45:
     crystal_end()
@@ -487,7 +520,6 @@ def situation_randomiser():
     doctor()
   elif Situation_randomiser <= 40:
     battle()
-  if gem_shards[2]["emerald_shard"] and gem_shards[1]["gold_shard"] and gem_shards[0]["diamond_shard"] == True:
+  if gem_shards[3]["emerald_shard"] == "Emerald_Shard" and gem_shards[2]["gold_shard"] == "Gold_Shard" and gem_shards[1]["diamond_shard"] == "Diamond_Shard":
     crystal_end()
 situation_randomiser()
-
